1f5dc1b662828e3d2d824d6fb72cdf36516837b2
Aer0m <47530448+Aer0m@users.noreply.github.com>
2019-09-15 15:43:14 +0300

    Create helloworld.cs

---
+++ helloworld.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace csharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Hello world!");
+			Console.WriteLine("Привет мир!");
+		}
+	}
+}
