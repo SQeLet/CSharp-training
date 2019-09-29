53511c39138cdecc19d1d36b378929b9bc5c3566
Aer0m <47530448+Aer0m@users.noreply.github.com>
2019-09-30 02:11:16 +0300

    Create MainWindow.xaml.cs

---
+++ Lab5/MainWindow.xaml.cs
@@ -0,0 +1,264 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab5
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Check_Click(object sender, RoutedEventArgs e)
+		{
+			if (t1.Text == "к")
+				t1.Background = Brushes.Green;
+			else
+				t1.Background = Brushes.Red;
+
+			if (t2.Text == "о")
+				t2.Background = Brushes.Green;
+			else
+				t2.Background = Brushes.Red;
+
+			if (t3.Text == "р")
+				t3.Background = Brushes.Green;
+			else
+				t3.Background = Brushes.Red;
+
+			if (t4.Text == "е")
+				t4.Background = Brushes.Green;
+			else
+				t4.Background = Brushes.Red;
+
+			if (t5.Text == "н")
+				t5.Background = Brushes.Green;
+			else
+				t5.Background = Brushes.Red;
+
+			if (t6.Text == "ь")
+				t6.Background = Brushes.Green;
+			else
+				t6.Background = Brushes.Red;
+
+			if (t7.Text == "л")
+				t7.Background = Brushes.Green;
+			else
+				t7.Background = Brushes.Red;
+
+			if (t8.Text == "о")
+				t8.Background = Brushes.Green;
+			else
+				t8.Background = Brushes.Red;
+
+			if (t9.Text == "д")
+				t9.Background = Brushes.Green;
+			else
+				t9.Background = Brushes.Red;
+
+			if (t10.Text == "о")
+				t10.Background = Brushes.Green;
+			else
+				t10.Background = Brushes.Red;
+
+			if (t11.Text == "н")
+				t11.Background = Brushes.Green;
+			else
+				t11.Background = Brushes.Red;
+
+			if (t12.Text == "т")
+				t12.Background = Brushes.Green;
+			else
+				t12.Background = Brushes.Red;
+
+			if (t13.Text == "а")
+				t13.Background = Brushes.Green;
+			else
+				t13.Background = Brushes.Red;
+
+			if (t14.Text == "б")
+				t14.Background = Brushes.Green;
+			else
+				t14.Background = Brushes.Red;
+
+			if (t15.Text == "и")
+				t15.Background = Brushes.Green;
+			else
+				t15.Background = Brushes.Red;
+
+			if (t16.Text == "ц")
+				t16.Background = Brushes.Green;
+			else
+				t16.Background = Brushes.Red;
+
+			if (t17.Text == "а")
+				t17.Background = Brushes.Green;
+			else
+				t17.Background = Brushes.Red;
+
+			if (t18.Text == "л")
+				t18.Background = Brushes.Green;
+			else
+				t18.Background = Brushes.Red;
+
+			if (t19.Text == "м")
+				t19.Background = Brushes.Green;
+			else
+				t19.Background = Brushes.Red;
+
+			if (t20.Text == "е")
+				t20.Background = Brushes.Green;
+			else
+				t20.Background = Brushes.Red;
+
+			if (t21.Text == "р")
+				t21.Background = Brushes.Green;
+			else
+				t21.Background = Brushes.Red;
+
+			if (t22.Text == "л")
+				t22.Background = Brushes.Green;
+			else
+				t22.Background = Brushes.Red;
+
+			if (t23.Text == "у")
+				t23.Background = Brushes.Green;
+			else
+				t23.Background = Brushes.Red;
+
+			if (t24.Text == "а")
+				t24.Background = Brushes.Green;
+			else
+				t24.Background = Brushes.Red;
+
+			if (t25.Text == "р")
+				t25.Background = Brushes.Green;
+			else
+				t25.Background = Brushes.Red;
+
+			if (t26.Text == "б")
+				t26.Background = Brushes.Green;
+			else
+				t26.Background = Brushes.Red;
+
+			if (t27.Text == "у")
+				t27.Background = Brushes.Green;
+			else
+				t27.Background = Brushes.Red;
+
+			if (t28.Text == "з")
+				t28.Background = Brushes.Green;
+			else
+				t28.Background = Brushes.Red;
+		
+
+
+
+		}
+
+		private void Clean_Click(object sender, RoutedEventArgs e)
+		{
+			t1.Text = "";
+			t1.Background = Brushes.White;
+
+			t2.Text = "";
+			t2.Background = Brushes.White;
+
+			t3.Text = "";
+			t3.Background = Brushes.White;
+
+			t4.Text = "";
+			t4.Background = Brushes.White;
+
+			t5.Text = "";
+			t5.Background = Brushes.White;
+
+			t6.Text = "";
+			t6.Background = Brushes.White;
+
+			t7.Text = "";
+			t7.Background = Brushes.White;
+
+			t8.Text = "";
+			t8.Background = Brushes.White;
+
+			t9.Text = "";
+			t9.Background = Brushes.White;
+
+			t10.Text = "";
+			t10.Background = Brushes.White;
+
+			t11.Text = "";
+			t11.Background = Brushes.White;
+
+			t12.Text = "";
+			t12.Background = Brushes.White;
+
+			t13.Text = "";
+			t13.Background = Brushes.White;
+
+			t14.Text = "";
+			t14.Background = Brushes.White;
+
+			t15.Text = "";
+			t15.Background = Brushes.White;
+
+			t16.Text = "";
+			t16.Background = Brushes.White;
+
+			t17.Text = "";
+			t17.Background = Brushes.White;
+
+			t18.Text = "";
+			t18.Background = Brushes.White;
+
+			t19.Text = "";
+			t19.Background = Brushes.White;
+
+			t20.Text = "";
+			t20.Background = Brushes.White;
+
+			t21.Text = "";
+			t21.Background = Brushes.White;
+
+			t22.Text = "";
+			t22.Background = Brushes.White;
+
+			t23.Text = "";
+			t23.Background = Brushes.White;
+
+			t24.Text = "";
+			t24.Background = Brushes.White;
+
+			t25.Text = "";
+			t25.Background = Brushes.White;
+
+			t26.Text = "";
+			t26.Background = Brushes.White;
+
+			t27.Text = "";
+			t27.Background = Brushes.White;
+
+			t28.Text = "";
+			t28.Background = Brushes.White;
+
+
+		}
+	}
+}
