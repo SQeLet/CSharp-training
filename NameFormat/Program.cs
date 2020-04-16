d05a974e6bea49c7cd1e6cc4d05fdbcc616d79a6
Aer0m <47530448+Aer0m@users.noreply.github.com>
2020-04-16 15:25:31 +0300

    Create Program.cs

---
+++ NameFormat/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+	class Username
+	{
+		static string name;
+
+		public static void user (string s)
+		{
+			name = s;
+		}
+
+		public static string Format1()
+		{
+			string[] name_f1 = name.Split();
+			return name_f1[0] + " " + name_f1[1][0] + "." + name_f1[2][0] + ".";
+		}
+
+		public static string Format2()
+		{
+			string[] name_f2 = name.Split();
+			return name_f2[1] + " " + name_f2[2] + " " + name_f2[0];
+		}
+
+
+
+
+		public static void Main(string[] args)
+		{
+			user("Ivanov Dmitry Petrovich");
+
+			Console.WriteLine(Format1());
+			Console.WriteLine(Format2());
+
+		}
+	}
+}
