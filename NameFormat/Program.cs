c41c982a756a6089b3488000261e46001598547a
Aer0m <47530448+Aer0m@users.noreply.github.com>
2020-04-16 15:29:25 +0300

    Update Program.cs

--- NameFormat/Program.cs
+++ NameFormat/Program.cs
@@ -17,12 +17,14 @@ namespace Test
 
 		public static string Format1()
 		{
+			name = name.Trim();
 			string[] name_f1 = name.Split();
 			return name_f1[0] + " " + name_f1[1][0] + "." + name_f1[2][0] + ".";
 		}
 
 		public static string Format2()
 		{
+			name = name.Trim();
 			string[] name_f2 = name.Split();
 			return name_f2[1] + " " + name_f2[2] + " " + name_f2[0];
 		}
